5c5b99682716dfa0f9032781ad70a390322585e9
Chris Mann <chris@lesgtrandsvoisins.com>
2025-05-13 18:36:44 +0200

    Structure for a login and dashboard

--- annuaire/admin.py
+++ annuaire/admin.py
@@ -1,3 +1,10 @@
+# admin.py
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("title", "source_name", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("title", "source_name", "description")
+
