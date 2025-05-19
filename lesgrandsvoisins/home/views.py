5086e69d6b50c75a91e7b6c93069d084f629971c
Chris Mann <chris@lesgtrandsvoisins.com>
2025-05-19 10:19:44 +0200

    Ajout d'applications sur fixtures et sur page d'acceuil

--- lesgrandsvoisins/home/views.py
+++ lesgrandsvoisins/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from lesgrandsvoisins.admin.models import Application
 
 def index_view(request):
-  return render(request, 'lesgrandsvoisins/home/index.html', {'message': 'Bienvenue'})
+  context = {'message': 'Bienvenue'}
+  context["apps"] = Application.objects.all().order_by("title")
+  return render(request, 'lesgrandsvoisins/home/index.html', context)
